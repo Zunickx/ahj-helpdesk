78b5d7e4dc09282a638c4a4247a237b5d4ee3cc4
Zunickx <Zunickx@gmail.com>
2024-02-20 21:12:30 +0300

    Create file style.css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import "./css/style.css";
 import "./js/app";
 
--- src/js/removeTicketWidget.js
+++ src/js/removeTicketWidget.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 function removeTicketCancelButtonHandler(mainContainer) {
   if (!mainContainer) return;
 
